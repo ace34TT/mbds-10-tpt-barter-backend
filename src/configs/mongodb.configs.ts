cc04cf679bdd338c118ecb50d82dd7c15a05f02b
Robsona Tsiory <robsonatsiory07@gmail.com>
2024-07-11 16:27:51 +0300

    [fix] mongodb connection

--- src/configs/mongodb.configs.ts
+++ src/configs/mongodb.configs.ts
@@ -1,25 +1,14 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const uri = process.env.MONGODB_URL || "";
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
 export async function runMongoDbConnection() {
   try {
-    await client.connect();
-    await client.db("troc").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
+    mongoose.connect(uri);
+  } catch (error) {
+    console.log(error);
   }
 }
