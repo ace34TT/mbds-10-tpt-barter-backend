28799c954b4cebc5c9506b296a5b81da5d253065
ace34TT <tafinasoa35@gmail.com>
2024-08-26 19:46:00 +0300

    [fix] update cors to be allowed from any host

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors({origin: true, optionsSuccessStatus: 200}));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
